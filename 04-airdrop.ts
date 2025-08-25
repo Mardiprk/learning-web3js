b65b0a66fa1a7dabfb04669a77550866bd24c215
Mardiprk <mk1659770@gmail.com>
2025-08-25 17:04:10 +0000

    airdrop

---
+++ 04-airdrop.ts
@@ -0,0 +1,30 @@
+import {LAMPORTS_PER_SOL, Connection, clusterApiUrl, Keypair} from "@solana/web3.js";
+
+async function airdropSOL(){
+    const secret = Uint8Array.from([
+        101, 175, 189, 242, 134, 171, 204, 246, 205, 129,  43,
+   90, 134, 155,  21,  15, 146, 169, 220, 178, 234, 184,
+   42, 186, 167, 108, 218, 138, 149, 181, 249,   2,  82,
+  235, 223,  83,  12,  59, 250, 177, 220, 146, 141,  74,
+    7, 141,  13, 246, 113,  47, 252,   7, 194, 193,  25,
+   84, 200, 248,   9, 193,  64,  73, 109,   6
+    ]);
+
+    const wallet = Keypair.fromSecretKey(secret);
+
+    console.log("WAllet address: ", wallet.publicKey.toBase58());
+
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+    //airdrop 1 SOL
+    console.log("⏳ Requestion Airdrop 1 SOL");
+    const sig = await connection.requestAirdrop(wallet.publicKey, 1 * LAMPORTS_PER_SOL);
+
+    await connection.confirmTransaction(sig, "confirmed");
+    console.log("🍀 Airdrop Confirmed:", sig);
+
+    const bal = await connection.getBalance(wallet.publicKey);
+    console.log("Balance {SOL}", bal / LAMPORTS_PER_SOL);
+}
+
+airdropSOL();
